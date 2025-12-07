50de16caeb4091c3547a279fd864bb23be8c4197
Ben <149116518+Bkallhoff99099@users.noreply.github.com>
2025-12-06 22:55:42 -0600

    I did a sillier

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,166 @@
+package frc.robot;
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathfindingCommand;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new Drivetrain. */
+  private final SwerveDrive swerveDrive;
+
+
+  public Drivetrain(File directory) {
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    try{
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(4);
+    }catch(Exception e){
+      System.out.println("RUH ROH REGGIE, NO SWERVE CONFIG");
+      throw new RuntimeException(e);
+    }
+    swerveDrive.setHeadingCorrection(false);
+    swerveDrive.setCosineCompensator(false);
+    swerveDrive.setAngularVelocityCompensation(true, true, 0.1);
+    //synchronizes encoders while not moving after delay
+    swerveDrive.setModuleEncoderAutoSynchronize(false, 1);
+
+  }
+  
+  public Command drive(DoubleSupplier xSpeed, DoubleSupplier ySpeed, DoubleSupplier rot, boolean fieldRelative){
+    swerveDrive.setHeadingCorrection(true);
+    return run(()->{
+    Translation2d scaledInputs = SwerveMath.scaleTranslation(new Translation2d(xSpeed.getAsDouble(), ySpeed.getAsDouble()), 0.8);
+
+    swerveDrive.drive(scaledInputs,rot.getAsDouble(),fieldRelative,false);
+    });
+  }
+
+  public Command driveFieldOriented(DoubleSupplier xSpeed, DoubleSupplier ySpeed, DoubleSupplier headingX, DoubleSupplier headingY){
+    swerveDrive.setHeadingCorrection(true);
+    return run(()->{
+    Translation2d scaledInputs = SwerveMath.scaleTranslation(new Translation2d(xSpeed.getAsDouble(), ySpeed.getAsDouble()), 0.8);
+
+    swerveDrive.driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(scaledInputs.getX(), scaledInputs.getY(), headingX.getAsDouble(), headingY.getAsDouble(), swerveDrive.getOdometryHeading().getRadians(), swerveDrive.getMaximumChassisVelocity()));
+    });
+  }
+
+  public Command driveToPose(Pose2d pose){
+    PathConstraints constraints = new PathConstraints(swerveDrive.getMaximumChassisVelocity(), 4, swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(720));
+    return AutoBuilder.pathfindToPose(pose, constraints, edu.wpi.first.units.Units.MetersPerSecond.of(0));
+  }
+
+  public void setUpPathplanner(){
+     // Load the RobotConfig from the GUI settings. You should probably
+    // store this in your Constants file
+    RobotConfig config;
+    try
+    {
+      config = RobotConfig.fromGUISettings();
+
+      final boolean enableFeedforward = true;
+      // Configure AutoBuilder last
+      AutoBuilder.configure(
+          this::getPose,
+          // Robot pose supplier
+          this::resetOdometry,
+          // Method to reset odometry (will be called if your auto has a starting pose)
+          this::getRobotVelocity,
+          // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+          (speedsRobotRelative, moduleFeedForwards) -> {
+            if (enableFeedforward)
+            {
+              swerveDrive.drive(
+                  speedsRobotRelative,
+                  swerveDrive.kinematics.toSwerveModuleStates(speedsRobotRelative),
+                  moduleFeedForwards.linearForces()
+                               );
+            } else
+            {
+              swerveDrive.setChassisSpeeds(speedsRobotRelative);
+            }
+          },
+          // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+          new PPHolonomicDriveController(
+              // PPHolonomicController is the built in path following controller for holonomic drive trains
+              new PIDConstants(5.0, 0.0, 0.0),
+              // Translation PID constants
+              new PIDConstants(5.0, 0.0, 0.0)
+              // Rotation PID constants
+          ),
+          config,
+          // The robot configuration
+          () -> {
+            // Boolean supplier that controls when the path will be mirrored for the red alliance
+            // This will flip the path being followed to the red side of the field.
+            // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+            var alliance = DriverStation.getAlliance();
+            if (alliance.isPresent())
+            {
+              return alliance.get() == DriverStation.Alliance.Red;
+            }
+            return false;
+          },
+          this
+          // Reference to this subsystem to set requirements
+                           );
+
+    } catch (Exception e)
+    {
+      // Handle exception as needed
+      e.printStackTrace();
+    }
+
+    //Preload PathPlanner Path finding
+    // IF USING CUSTOM PATHFINDER ADD BEFORE THIS LINE
+    PathfindingCommand.warmupCommand().schedule();
+  }
+
+  public Pose2d getPose(){
+    return swerveDrive.getPose();
+  }
+
+  public void resetOdometry(Pose2d InitialHolonomicPose){
+    swerveDrive.resetOdometry(InitialHolonomicPose);
+  }
+  
+  public ChassisSpeeds getRobotVelocity(){
+    return swerveDrive.getRobotVelocity();
+  }
+
+  public void resetHeading(){
+    swerveDrive.resetDriveEncoders();
+  }
+
+public SwerveDrive getSwerveDrive(){
+  return swerveDrive;
+}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,33 @@
 
 package frc.robot;
 
+import java.io.File;
+
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import swervelib.SwerveInputStream;
 
 public class RobotContainer {
+
+  private CommandXboxController m_driveController = new CommandXboxController(0);
+  private Drivetrain swerve = new Drivetrain(new File(Filesystem.getDeployDirectory(), "Swerve"));
   public RobotContainer() {
+    SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerve.getSwerveDrive(), ()-> m_driveController.getLeftY() * -1, ()->m_driveController.getLeftX() * -1).withControllerRotationAxis(m_driveController::getRightX).deadband(.3).scaleTranslation(0.8).allianceRelativeControl(true);
+    SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(m_driveController::getRightX, m_driveController::getRightY).headingWhile(true);
     configureBindings();
+    swerve.setDefaultCommand(swerve.drive(
+                            ()->m_driveController.getLeftX(),
+                            ()->m_driveController.getLeftY(),   
+                            ()->m_driveController.getRightX(),  
+                            true));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
